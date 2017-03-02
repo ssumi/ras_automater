f867cef973889ac9c0cdad962e46ae8c1ee012c5
admin <admin>
2017-03-01 22:10:32 -0500

    Run the program

---
+++ run_RAS_anacostia.cmd
@@ -0,0 +1,4 @@
+  
+  # "C:\Program Files (x86)\HEC\HEC-RAS\5.0.3\Ras.exe" "C:\Users\admin\Desktop\REALTIME_RAS\Seth_Anacostia\Anacostia_FEMA.prj"-c
+            
+  "C:\Program Files (x86)\HEC\HEC-RAS\5.0.3\Ras.exe" "C:\Users\admin\Desktop\REALTIME_RAS\Seth_Anacostia\Anacostia_FEMA.prj"
